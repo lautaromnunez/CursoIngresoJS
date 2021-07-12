e4c43c0274fa43183d6f087d1edcfcd439fb6de8
lautaromnunez <lautaromnunez02@gmail.com>
2021-07-12 19:45:03 -0300

    Ejercicios IF

--- 2-InstruccionIf/jsInstruccionIF-02.js
+++ 2-InstruccionIf/jsInstruccionIF-02.js
@@ -1,7 +1,13 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	let edad;
+
+	edad = document.getElementById("txtIdEdad").value;
+	edad = parseInt(edad)
+	if(edad >= 18)
+	{
+		alert("Usted es mayor de edad");
+	}
 	
 
-}//FIN DE LA FUNCIÓN
+}
--- 2-InstruccionIf/jsInstruccionIF-03.js
+++ 2-InstruccionIf/jsInstruccionIF-03.js
@@ -1,6 +1,16 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	let edad;
 
-}//FIN DE LA FUNCIÓN
+	edad = document.getElementById("txtIdEdad").value;
+	edad = parseInt(edad)
+	if(edad >= 18)
+	{
+		alert("Usted es mayor de edad");
+	}
+	else
+	{
+		alert("Usted es menor de edad");
+	}
+
+}
