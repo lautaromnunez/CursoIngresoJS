86d50e3432a2f7f97cd2e16fee91dc77d62cb77d
lautaromnunez <lautaromnunez02@gmail.com>
2021-07-12 22:31:14 -0300

    Ejercicios 12/7 IF

--- 2-InstruccionIf/jsInstruccionIF-06.js
+++ 2-InstruccionIf/jsInstruccionIF-06.js
@@ -1,8 +1,27 @@
+/* Al ingresar una edad menor a 18 años y un estado civil distinto a "Soltero",
+ mostrar el siguiente mensaje: 'Es muy pequeño para NO ser soltero.' */
+
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	let edad;
+
+	edad = document.getElementById("txtIdEdad").value;
+	edad = parseInt(edad)
+	if(edad >17)
+	{
+		alert("Usted es mayor de edad");
+	}
+	else
+	{
+		if(edad > 12 && edad <18)
+		{
+			alert("Usted es adolescente");
+		}
+		else
+		{
+		  alert("Usted es menor de edad");
+		}
+	}
 
 
-
-}//FIN DE LA FUNCIÓN
+}//FIN DE LA FUNCIÓN 
